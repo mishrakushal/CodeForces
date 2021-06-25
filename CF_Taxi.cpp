1143769a22c3c04d50c405bb68002be8ec0ee34b
Kushal Mishra <kuskalm@gmail.com>
2021-06-25 18:17:13 +0530

    changed logic

--- CF_Taxi.cpp
+++ CF_Taxi.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 using namespace std;
 
+#define ll long long
+
 int main()
 {
-    int n;
+    ll int n;
     cin >> n;
 
+    ll int k, count[5] = {0}, i;
+    for (i = 0; i < n; ++i)
+    {
+        cin >> k;
+        count[k]++;
+    }
+
+    ll int taxi = 0;
+    taxi += count[4] + count[3] + (count[2] / 2);
+    count[1] -= count[3];
     
+    if (count[2] % 2 == 1)
+    {
+        taxi++;
+        count[1] -= 2;
+    }
 
+    if (count[1] > 0)
+        taxi += ((count[1] + 3) / 4);
 
+    cout << taxi << endl;
 
     return 0;
 }
