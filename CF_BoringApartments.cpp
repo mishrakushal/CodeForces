dd2ee404b72950683e8e619c05cb93cbf4c89d9f
Kushal Mishra <kuskalm@gmail.com>
2021-07-06 20:06:34 +0530

    reduced time complexity

--- CF_BoringApartments.cpp
+++ CF_BoringApartments.cpp
@@ -13,7 +13,18 @@ int main()
         cin >> x;
 
         int i = 1, count = 0, temp = log10(x) + 1;
-        while (i <= (x % 10))
+        int digit = x % 10;
+        count = ((digit - 1) * 10) + ((temp) * (temp + 1) / 2);
+        cout << count << endl;
+    }
+
+
+    return 0;
+}
+
+/*
+
+while (i <= (x % 10))
         {
             if (i < (x % 10))
                 count += 10;
@@ -21,9 +32,5 @@ int main()
                 count += ((temp) * (temp + 1)) / 2;
             i++;
         }
-        cout << count << endl;
-    }
 
-
-    return 0;
-}
+*/
