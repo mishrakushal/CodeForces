48497dd24a0d828343dfb230dc905eb6a04979ab
Kushal Mishra <kuskalm@gmail.com>
2022-04-05 21:45:30 +0530

    solved 1475A - Odd Divisor

---
+++ CF_OddDivisor.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main () {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		ll n;
+		cin >> n;
+		
+		if (n & (n - 1)) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	return 0;
+}
