23b7779a49fa47367901a818e4d0544b92718c99
Kushal Mishra <kuskalm@gmail.com>
2022-04-05 21:46:57 +0530

    solved 1370A - Maximum GCD

---
+++ CF_MaxGCD.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main () {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		ll n;
+		cin >> n;
+		
+		cout << n / 2 << "\n";
+	}
+	return 0;
+}
